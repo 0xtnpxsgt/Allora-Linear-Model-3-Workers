3149d0ad69b582c4fbc7e9b5d2517fe51b1eee36
0xtnpxsgt <66821987+0xtnpxsgt@users.noreply.github.com>
2024-07-29 10:58:25 +0300

    Update allora-3W.sh

--- allora-3W.sh
+++ allora-3W.sh
@@ -325,9 +325,6 @@ volumes:
   head-data:
 EOL
 
-    echo -e "\e[1;34m===== Running Docker Compose =====\e[0m"
-    docker-compose build
-
     echo -e "\e[1;32m======= Setup Complete! Proceed to the Next Step =======\e[0m"
     display_message
     echo "docker" > ~/.docker_setup_stage
